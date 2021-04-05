ab4252f900c3ef42da0a547ce33cadec3bc4c4f7
Viera Garcia <79938481+vierafga@users.noreply.github.com>
2021-04-05 10:16:34 -0700

    adding file

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,8 @@ import os
 import csv
 #file to read to:
 csvpath = os.path.join("Resources", "budget_data.csv")
+#file to output
+file_to_output = os.path.join("Analysis", "pybank_analysis.txt")
 
 print("Financial Analysis")
 print("----------------------------")
@@ -10,6 +12,7 @@ print("----------------------------")
 # Lists to store data
 profit_losses = []
 date = []
+changes = []
 
 #Open cvs
 with open(csvpath, newline='') as budgetfile:
@@ -19,10 +22,10 @@ with open(csvpath, newline='') as budgetfile:
     for row in csvreader:
         profit_losses.append(int(row[1]))
         date.append(row[0])
-    
+        changes.append(int(row[1]))
     total_sum = sum(profit_losses)
     total_months = len(date)
-        # average_change = round(int(total_sum)/ int(total_months), 2))
+    #average_change = round(int(total_sum)/ int(total_months), 2))
 
     print("Total Months: " + str(total_months))
     print("Total: " + '${}'.format(total_sum))
@@ -32,6 +35,8 @@ with open(csvpath, newline='') as budgetfile:
         #print("Greatest Decrease in Profits:")
 
 #print the analysis to the terminal and export a text file with the results.
-#with open(output_file, "w", newline="") as datafile:
-    #writer = csv.writer(datafile)
-       
+with open(file_to_output, "w") as txt_file:
+    txt_file.write("this is an example")
+    
+#with open(file_to_output, "a") as txt_file:
+    #txt_file.write(output)
