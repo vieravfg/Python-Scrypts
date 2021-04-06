8db68be326db34aa590c441465d1625bf3bbb35d
Viera Garcia <79938481+vierafga@users.noreply.github.com>
2021-04-05 17:22:50 -0700

    adding more

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,10 +5,6 @@ import csv
 csvpath = os.path.join("Resources", "budget_data.csv")
 #file to output
 file_to_output = os.path.join("Analysis", "pybank_analysis.txt")
-
-print("Financial Analysis")
-print("----------------------------")
-
 # Lists to store data
 profit_losses = []
 date = []
@@ -22,21 +18,31 @@ with open(csvpath, newline='') as budgetfile:
     for row in csvreader:
         profit_losses.append(int(row[1]))
         date.append(row[0])
-        changes.append(int(row[1]))
+
+    for x in range(1,len(profit_losses)):
+        changes.append(profit_losses[x]-profit_losses[x-1])
+
     total_sum = sum(profit_losses)
     total_months = len(date)
-    #average_change = round(int(total_sum)/ int(total_months), 2))
 
-    print("Total Months: " + str(total_months))
-    print("Total: " + '${}'.format(total_sum))
+    changes_sum = sum(changes)
+    average_change = round((changes_sum)/ (int(len(changes))), 2)
+
+output = (
+    "Financial Analysis\n"
+    "----------------------------\n"
+    f"Total Months: {total_months}\n"
+    "Total: " + '${}'.format(total_sum) + "\n"
+    "Average Change: " + '${}'.format(average_change))
 
         #print("Average:" + str(average_change))
         #print("Greatest Increase in Profits:")
         #print("Greatest Decrease in Profits:")
 
 #print the analysis to the terminal and export a text file with the results.
+print(output)
 with open(file_to_output, "w") as txt_file:
-    txt_file.write("this is an example")
-    
+    txt_file.write(output)
+  #  f"Approximate Word Count: {word_count}\n"
 #with open(file_to_output, "a") as txt_file:
     #txt_file.write(output)
